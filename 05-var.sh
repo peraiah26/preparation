edc0716cc369cbd57879b7c4373010c70ad41e4b
name <peraiahtellagorla92@gmail.com>
2026-07-07 15:04:14 +0530

    preparation

--- 05-var.sh
+++ 05-var.sh
@@ -8,3 +8,7 @@ echo "PLEASE ENTER THE YOUR PASSWORD:"
 read -s password
 echo " YOUR PASSWORD IS: $password"
 
+echo "Please enter your PIN number"
+read -s PIN_NUMBER # here PIN_NUMBER is variable
+
+echo "PIN entered is: $PIN_NUMBER"
