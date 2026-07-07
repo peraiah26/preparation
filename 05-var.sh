c45037cddb6007a7b1f16742e6fc56cf55785bba
name <peraiahtellagorla92@gmail.com>
2026-07-07 14:59:40 +0530

    preparation

---
+++ 05-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "HI TELL ME YOUR NAME PLEASE:"
+read name
+
+echo "PLEASE ENTER THE YOUR PSAAWORD:"
+read -s password
+
+echo " NAME IS: $name"
+echo " YOUR PASSWORD IS: $password
+
