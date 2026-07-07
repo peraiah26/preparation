7f4c2d56333d19d3a566395a6533ce52f1790e81
name <peraiahtellagorla92@gmail.com>
2026-07-07 14:49:17 +0530

    preparation

---
+++ 03-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=raju
+PERSON2=king
+
+
+echo "$PERSON1:: Hi $PERSON2 how are you"
+echo "$PERSON2:: I am doing good $PERSON1 wt about you"
+echo "$PERSON1:: I fine $PERSON2 what else brother"
+echo "$PERSON2:: Nothing $PERSON1 you can tell me brother"
