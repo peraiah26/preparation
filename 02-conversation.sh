b8ef6c134ac40f09fc907f55e1077d53bf1fadaa
name <peraiahtellagorla92@gmail.com>
2026-07-07 14:44:28 +0530

    preparation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "PERAIAH:: Hi venkat how are you"
+echo "VENKAT:: I am doing good PERAIAH wt about you"
+echo "PERAIAH:: I fine VENKAT what else brother"
+echo "VENLAT:: Nothing PERAIAH you can tell me brother"
