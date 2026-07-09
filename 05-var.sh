7012d0fde05369aa43faa015e763cb7e70c0a73a
name <peraiahtellagorla92@gmail.com>
2026-07-09 14:23:42 +0530

    preparation

--- 05-var.sh
+++ 05-var.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo -n "HI TELL ME YOUR NAME PLEASE"
+echo "HI TELL ME YOUR NAME PLEASE"
 read name
 
 echo  " NAME IS: $name"
 
-echo -n "PLEASE ENTER THE YOUR PASSWORD"
+echo "PLEASE ENTER THE YOUR PASSWORD"
 read -s password
 
 echo " YOUR PASSWORD IS: $password"
 
-echo -n "Please enter your PIN number"
+echo "Please enter your PIN number"
 read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
