e9f7a8a96ee3bff47134fd7bd756c98998b3fa11
name <peraiahtellagorla92@gmail.com>
2026-07-13 15:48:17 +0530

    preparation

--- 09-spl-vars.sh
+++ 09-spl-vars.sh
@@ -6,3 +6,13 @@ C=3
 sum=$(($A+$B+C))
 
 echo " sum of three numbers is: $sum"
+
+
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "All leaders: ${LEADERS[*]}"
+echo "All leaders: ${LEADERS[#]}"
+
+#echo "First Leader: ${LEADERS[0]}"
+#echo "First Leader: ${LEADERS[10]}"
