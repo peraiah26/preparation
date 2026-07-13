1809aa6dce58c4037358a9d71a5203f6c3394972
name <peraiahtellagorla92@gmail.com>
2026-07-13 16:00:53 +0530

    preparation

---
+++ 10-condition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUMBER=$
+
+if [ $NUMBER -gt 10 ]; then
+
+echo "Given number is greater than 10"
+
+elif [ $NUMBER -eq 10 ]; then 
+
+echo " Given number is equal to 10"
+
+else
+
+echo " Given number is less than 10"
+
+fi
