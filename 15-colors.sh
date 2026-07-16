6cd80ffa82fcce8c8c44a9577acf7e74653fbaf9
name <peraiahtellagorla92@gmail.com>
2026-07-16 16:46:42 +0530

    preparation

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo -e "${G}peraiah is a devops engineer${N}"
+echo -e "${R}peraiah is a devops engineer${N}"
+echo -e "${Y}peraiah is a devops engineer${N}"
 echo -e "${G}peraiah is a devops engineer"
