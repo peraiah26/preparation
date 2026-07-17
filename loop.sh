f5f120936f4c9998b6ceca88caa5f46748b9fe72
name <peraiahtellagorla92@gmail.com>
2026-07-17 16:27:46 +0530

    preparation

--- loop.sh
+++ loop.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-for i in 1 2 3 4 5
-do 
-    echo "$i"
-done
+# for i in 1 2 3 4 5
+# do 
+#     echo "$i"
+# done
 
-for name in peraiah rambabu ganesh
+# for name in peraiah rambabu ganesh
+# do
+#     echo "Hello $name"
+# done
+
+for i in {1..10..2}
 do
-    echo "Hello $name"
+    echo $in
 done
-
-        
