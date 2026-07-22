87ef5c8229678d035e6349b549643da20abaeb6d
name <peraiahtellagorla92@gmail.com>
2026-07-22 21:13:17 +0530

    preparation

--- graterthan.sh
+++ graterthan.sh
@@ -4,10 +4,10 @@ A=20
 B=6
 C=70
 
-if [$A -ge $B] && [$A -ge $C]
+if [ $A -ge $B ] && [ $A -ge $C ]
 then
     echo " A IS BIG"
-elif [$B -ge $A] && [$B -ge $C]
+elif [ $B -ge $A ] && [ $B -ge $C ]
 then
     echo "B IS BIG"
 else
